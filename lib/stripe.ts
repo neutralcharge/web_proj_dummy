b49251f3bc5664ce3e9139124b753de29162cf39
neutralcharge <1oneplushivam@gmail.com>
2025-02-09 17:24:54 +0530

    a

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,11 +1,1 @@
-import Stripe from "stripe"
-
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY is missing. Please set the environment variable.")
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2023-10-16",
-  typescript: true,
-})
 
